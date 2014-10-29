bd990c7c6ab5ee0d093378cfbaf0a883c697cb4d
Masayuki Morisaki <masayuki144@gmail.com>
2014-10-29 09:40:10 +0900

    各種アプリケーションをbrewでインストールするシェルを作成

---
+++ dotfiles/brew.sh
@@ -0,0 +1,30 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+brew install git
+brew install git-flow
+brew install node
+brew install tig
+brew install subversion
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install alfred
+brew cask install dropbox
+brew cask install evernote
+brew cask install keyremap4mackbook
+brew cask install skitch
+brew cask install sequel-pro
+brew cask install sourcetree
+brew cask install macvim
+brew cask install filezilla
+brew cask install skype
+brew cask install dash
+brew cask install mou
+brew cask install slack
+brew cask install sparrow
+brew cask install xtrafinder
+brew cask install mplayerx
+
+brew cask alfred link
