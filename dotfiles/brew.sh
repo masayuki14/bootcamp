4d76b48819315ff9ca6979f0b90e1b7cdae6cc46
Masayuki Morisaki <morisaki@Masayuki-no-iMac.local>
2016-09-01 10:12:39 +0900

    brewの内容を見直す

--- dotfiles/brew.sh
+++ dotfiles/brew.sh
@@ -1,43 +1,43 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap phinze/homebrew-cask
+brew tap caskroom/cask
 brew install brew-cask
 
+brew install fish
 brew install git
 brew install git-flow
-brew install node
-brew install tig
-brew install fish
-brew install subversion
-brew install tree
 brew install nkf
+brew install node
+#brew install subversion
+brew install tig
+brew install tree
 brew install zsh-completions
 
-brew cask install iterm2
-brew cask install google-chrome
+#brew cask install filezilla
+#brew cask install keyremap4mackbook
+#brew cask install mou
+#brew cask install mplayerx
+#brew cask install xtrafinder
 brew cask install alfred
+brew cask install atom
+brew cask install coteditor
+brew cask install dash
 brew cask install dropbox
 brew cask install evernote
-brew cask install keyremap4mackbook
-brew cask install skitch
-brew cask install sequel-pro
-brew cask install sourcetree
-brew cask install macvim
-brew cask install filezilla
-brew cask install skype
-brew cask install dash
-brew cask install mou
-brew cask install slack
-brew cask install sparrow
-brew cask install xtrafinder
-brew cask install mplayerx
-brew cask install libreoffice
-brew cask install coteditor
-brew cask install virtualbox
-brew cask install vagrant
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install iterm2
 brew cask install karabiner
-
-brew cask alfred link
-
+brew cask install libreoffice
+brew cask install macvim
+brew cask install sequel-pro
+brew cask install skitch
+brew cask install skype
+brew cask install slack
+brew cask install sourcetree
+brew cask install sparrow
+brew cask install unity
+brew cask install vagrant
+brew cask install virtualbox
 
 brew tap peco/peco
 brew install peco
