6b9453482cd44f075589f0220ee1abc38bb9c67d
Masayuki Morisaki <masayuki144@gmail.com>
2022-03-14 11:07:37 +0900

    brew のアプリケーションを整理

--- dotfiles/brew.sh
+++ dotfiles/brew.sh
@@ -1,13 +1,13 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/cask
-brew install brew-cask
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#brew tap caskroom/cask
+#brew install brew-cask
 
-brew install fish
+#brew install fish
 brew install ghq
 brew install git
-brew install git-flow
+#brew install git-flow
 brew install jq
-brew install nkf
+#brew install nkf
 brew install node
 #brew install subversion
 brew install peco
@@ -15,45 +15,53 @@ brew install rbenv
 brew install rbenv-gemset
 brew install rbenv-gem-rehash
 brew install ruby-build
-brew install terminal-notifier
+#brew install terminal-notifier
 brew install tig
 brew install tree
 brew install zsh-completions
 brew install libxml2 libxslt
+
+brew install docbackground-musicker
+brew install background-music
+brew install boostnote
+brew install visual-studio-code
+brew install mysqlworkbench
+brew install mongodb-compass
 
 #brew cask install filezilla
 #brew cask install keyremap4mackbook
 #brew cask install mou
 #brew cask install mplayerx
 #brew cask install xtrafinder
-brew cask install alfred
-brew cask install atom
+brew install alfred
+#brew cask install atom
 #brew cask install coteditor
-brew cask install dash
-brew cask install dropbox
-brew cask install evernote
-brew cask install google-chrome
-brew cask install google-japanese-ime
+brew install dash
+brew install dropbox
+brew install evernote
+brew install google-chrome
+brew install google-japanese-ime
 #brew cask install google-nik-collection
-brew cask install iterm2
-brew cask install karabiner-elements
+brew install iterm2
+brew install karabiner-elements
 #brew cask install libreoffice
-brew cask install macvim
-brew cask install sequel-pro
-#brew cask install skitch
-brew cask install skype
-brew cask install slack
-brew cask install sourcetree
-brew cask install vagrant
-brew cask install virtualbox
-
+brew install macvim
+brew install sequel-pro
+brew install skitch
+brew install skype
+brew install slack
+brew install sourcetree
+brew install vagrant
+brew install virtualbox
+brew install virtualbox-extension-pack
 brew install peco
 
-brew tap caskroom/fonts
-brew cask install font-droid-sans-mono
-brew cask install font-fira-mono
-brew cask install font-ricty-diminished
-brew cask install font-roboto-mono
-brew cask install font-dejavu-sans
-font-source-han-code-jp
-font-myrica
+#brew tap caskroom/fonts
+brew install font-droid-sans-mono
+brew install font-fira-mono
+brew install font-ricty-diminished
+brew install font-roboto-mono
+brew install font-dejavu-sans 
+brew install font-source-han-code-jp 
+brew install font-myrica
+brew install font-droid-sans-mono-nerd-font	
